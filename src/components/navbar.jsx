eb6272ef887da14eb2c6d11c3f741775dcd2ba64
Aryan <aryan03pandey@gmail.com>
2024-10-28 23:36:23 +0530

    Fixed Link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,12 +7,12 @@ const Navbar = () => {
         <nav >
             <div className="h-[100px] bg-black flex items-center justify-between px-10">
                 <div className="flex  items-center gap-2">
-                    <h1 className="font-bold text-white text-[30px]">TUF</h1>
+                    <h1 className="font-bold text-white text-[30px]">AlgoZenith</h1>
                     <div className="bg-[#4096ff] w-[8px] h-[40px] "></div>
                 </div>
 
                 <div>
-                    <Link to="https://takeuforward.org/plus">
+                    <Link to="https://maang.in/">
                         <RiseOutlined className="text-white text-[40px]" />
                     </Link>
                 </div>
